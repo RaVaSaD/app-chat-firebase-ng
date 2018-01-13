e784b8bb6b4bf11dde6dd66959dfc23c411538d1
NENES-PC2\Admin <ravasad@gmail.com>
2018-01-13 13:39:16 +0100

    Initial commit.

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-q0e4236NecCrbrq4gmI6MMY2DsnpdAU",
+  authDomain: "firechat-74ac3.firebaseapp.com",
+  databaseURL: "https://firechat-74ac3.firebaseio.com",
+  projectId: "firechat-74ac3",
+  storageBucket: "firechat-74ac3.appspot.com",
+  messagingSenderId: "886040738863"
+};
